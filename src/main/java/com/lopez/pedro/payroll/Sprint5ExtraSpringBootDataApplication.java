23c8ba080c3ce8a6091d22b503fb2b4b79ed2850
Pedro Lopez <p.lopez.ch.hr@hotmail.com>
2024-01-10 12:31:20 +0100

    tries CommandLineRunner class passing a repository as parameter and use it to fulfill data; declares first method sign of the SucursalRepository interface;

--- src/main/java/com/lopez/pedro/payroll/Sprint5ExtraSpringBootDataApplication.java
+++ src/main/java/com/lopez/pedro/payroll/Sprint5ExtraSpringBootDataApplication.java
@@ -1,13 +1,25 @@
 package com.lopez.pedro.payroll;
 
+import com.lopez.pedro.payroll.entity.Sucursal;
+import com.lopez.pedro.payroll.repository.SucursalRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class Sprint5ExtraSpringBootDataApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(Sprint5ExtraSpringBootDataApplication.class, args);
+	public static void main(String[] args) {SpringApplication.run(Sprint5ExtraSpringBootDataApplication.class, args);}
+
+	@Bean
+	CommandLineRunner initDataDemo (SucursalRepository surcursalRepository) {
+		return args -> {
+			surcursalRepository.save(new Sucursal("Collegium", "Harmonic"));
+			surcursalRepository.save(new Sucursal("Brio", "Fully"));
+		};
 	}
+
+
 
 }
--- src/main/java/com/lopez/pedro/payroll/repository/SucursalRepository.java
+++ src/main/java/com/lopez/pedro/payroll/repository/SucursalRepository.java
@@ -3,8 +3,14 @@ package com.lopez.pedro.payroll.repository;
 import com.lopez.pedro.payroll.entity.Sucursal;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+import java.util.List;
 
 @RepositoryRestResource
 public interface SucursalRepository extends CrudRepository<Sucursal, Integer> {
+
+    List<Sucursal> findByName (@Param("q")String name);
+
 }
